aa121edc18c70fd7d848d26a8213cdd584633616
Fikran Akbar <fikranakbar756@gmail.com>
2020-05-13 19:53:02 +0700

    CRASH when click item to show detail

--- app/src/main/java/com/chessporg/animeprofilepicture/DetailActivity.java
+++ app/src/main/java/com/chessporg/animeprofilepicture/DetailActivity.java
@@ -2,7 +2,12 @@ package com.chessporg.animeprofilepicture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent in = getIntent();
+        int index = in.getIntExtra("com.chessporg.ITEM_INDEX", -1);
+        if(index > -1){
+            int pic = getImg(index);
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            scaleImg(imageView, pic);
+        }
+    }
+
+    public int getImg(int index){
+        switch (index){
+            case 0: return R.drawable.hinata;
+            case 1: return R.drawable.tanjirou;
+            case 2: return R.drawable.kirito;
+            case 3: return R.drawable.yugi;
+            case 4: return R.drawable.kageyama;
+            case 5: return R.drawable.naruto;
+            default: return -1;
+        }
+    }
+
+    public void scaleImg(ImageView img, int pic){
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imgWidth = options.outWidth;
+        int scrWidth = screen.getWidth();
+
+        if(imgWidth > scrWidth){
+            int ratio = Math.round((float) imgWidth / (float) scrWidth);
+            options.inSampleSize = ratio;
+        }
+
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), pic, options);
+        img.setImageBitmap(scaledImg);
     }
 }
